a922d1c4dd37f129db80e30f41fe5e0625f7d3c6
George Thomas <georgesilverbeech@gmail.com>
2024-02-10 22:19:57 +0000

    Initial partial commit

---
+++ src/main/java/factorial/Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial {
+    public static int factorial(int input) {
+        int result = 1;
+        for (i = 1; i <= input; i++) {
+            result *= 1;
+        }
+        return result;
+    }
+
+    public static int recursiveFactorial(int input){
+
+    }
+}

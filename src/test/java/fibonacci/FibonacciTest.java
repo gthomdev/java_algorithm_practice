8efef23a40d35325c885c4fdc8d198be5b7b55bb
George Thomas <georgesilverbeech@gmail.com>
2024-02-13 20:08:56 +0000

    Update

--- src/test/java/fibonacci/FibonacciTest.java
+++ src/test/java/fibonacci/FibonacciTest.java
@@ -1,6 +1,14 @@
 package fibonacci;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.jupiter.api.Test;
-//import fibonacci.Fibonacci;
-//
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import factorial.Factorial;
+
+public class FibonacciTest {
+    @Test
+    public void factorialShouldReturnCorrectFactorial() {
+        int input = 10;
+        int expectedResult = 3628800;
+        assertEquals(expectedResult, Factorial.factorial(input));
+    }
+}
